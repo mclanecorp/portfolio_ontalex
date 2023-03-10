dac8c725f9d05549d3c4250a38c61687528dfe5e
mclanecorp <darkmienworld@gmail.com>
2023-03-10 14:12:27 +0100

    premier commit debut du projet

---
+++ app.js
@@ -0,0 +1,16 @@
+const menu = document.getElementById("menu");
+const close = document.getElementById("close");
+const hamburger = document.getElementById("hamburger");
+const nav_bar = document.getElementById("nav_bar");
+
+hamburger.addEventListener("click", () => {
+  if (menu.style.display === "block") {
+    menu.style.display = "none";
+    close.style.display = "block";
+    nav_bar.style.display = "flex";
+  } else {
+    menu.style.display = "block";
+    close.style.display = "none";
+    nav_bar.style.display = "none";
+  }
+});
