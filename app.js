c5a14df389364b43c291881fe0c9db6053d87b3b
mclanecorp <darkmienworld@gmail.com>
2023-03-10 20:21:09 +0100

    50% travail fait

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const close = document.getElementById("close");
 const hamburger = document.getElementById("hamburger");
 const nav_bar = document.getElementById("nav_bar");
 
+menu.style.display = "block";
+close.style.display = "none";
+nav_bar.style.display = "none";
+
 hamburger.addEventListener("click", () => {
   if (menu.style.display === "block") {
     menu.style.display = "none";
